10d57c866bf7579058d5c305e8b853874bfced11
sam <sampatry8@gmail.com>
2026-03-24 19:40:02 -0400

    in progress

--- src/navigation_pkg/launch/nav_manipulator.launch.py
+++ src/navigation_pkg/launch/nav_manipulator.launch.py
@@ -1,10 +1,18 @@
+# import os
+# from ament_index_python.packages import get_package_share_directory
+# from launch import LaunchDescription
+# from launch.actions import IncludeLaunchDescription
+# from launch.launch_description_sources import PythonLaunchDescriptionSource
+# from launch.substitutions import PathJoinSubstitution
+# from launch_ros.substitutions import FindPackageShare
+
+#imports from example
 import os
 from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
 from launch.actions import IncludeLaunchDescription
 from launch.launch_description_sources import PythonLaunchDescriptionSource
-from launch.substitutions import PathJoinSubstitution
-from launch_ros.substitutions import FindPackageShare
+from launch_ros.actions import Node
 
 def generate_launch_description():
     map_file = os.path.join(
